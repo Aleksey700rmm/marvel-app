b5767c6fd588dff98247948a7afba894fe9c541a
Aleksey700rmm <92338644+Aleksey700rmm@users.noreply.github.com>
2022-08-01 17:16:21 +0300

    ComicsList added

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -2,17 +2,16 @@ import { useState, useEffect } from 'react';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import mjolnir from '../../resources/img/mjolnir.png';
-import MarvelService from '../../services/MarvelService';
+import useMarvelService from '../../services/MarvelService';
 import './randomChar.scss';
 
 
 const RandomChar = (props) => {
 
     const [char, setChar] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(false);
+    
 
-    const marvelService = new MarvelService();
+    const {loading, error, getCharacter, clearError} = useMarvelService();
 
     useEffect(() => {
         updateChar();
@@ -24,27 +23,14 @@ const RandomChar = (props) => {
     }, [])
 
     const onCharLoaded = (char) => {
-        setLoading(false);
         setChar(char); 
-    }
-
-    const onCharLoading = () => {
-        setLoading(true);
-    }
-
-    const onError = () => {
-        setError(true);
-        setLoading(false);
-        
     }
     
     const updateChar = () => {
+        clearError();
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
-        onCharLoading();
-        marvelService
-            .getCharacter(id)
-            .then(onCharLoaded)
-            .catch(onError);
+        getCharacter(id)
+            .then(onCharLoaded);
     }
 
     const errorMessage = error ? <ErrorMessage/> : null;
